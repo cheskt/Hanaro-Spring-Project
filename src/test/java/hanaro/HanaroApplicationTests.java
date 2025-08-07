aac3dee2b3c4e48c96d47bfd14c4aea718205ed4
cheskt <andy9793@naver.com>
2025-08-07 17:42:16 +0900

    first commit

---
+++ src/test/java/hanaro/HanaroApplicationTests.java
@@ -0,0 +1,13 @@
+package hanaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
