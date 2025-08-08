2eae8fdeb413b1530d21bc0a32d805a486c1963b
cheskt <andy9793@naver.com>
2025-08-08 17:43:20 +0900

    쿼리문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into member(email, password, role, createdAt) value('hanaro', '12345678', 'ADMIN', now());
+insert into Member(email, password, role, createdAt) value('hanaro', '12345678', 'ADMIN', now());
 
-select * from member;
+select * from Member;
 
-insert into item(itemName, price, stock) values ('1번 상품', 10000, 1),
+insert into Item(itemName, price, stock) values ('1번 상품', 10000, 1),
                                                 ('2번 상품', 20000, 2),
                                                 ('3번 상품', 30000, 2)
