f429bfe9a88117697cc97510bfc588e590f6154d
cheskt <andy9793@naver.com>
2025-08-08 17:32:47 +0900

    member controller 설명 추가

--- src/main/java/hanaro/member/controller/MemberController.java
+++ src/main/java/hanaro/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import hanaro.member.dto.MemberDTO;
 import hanaro.member.dto.SignUpDTO;
 import hanaro.member.service.MemberService;
 import hanaro.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,19 +27,22 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "유저 등록", description = "회원가입합니다")
     @PostMapping("/signup")
     public ResponseEntity<MemberDTO> signUpMember(@RequestBody @Valid SignUpDTO requestDTO) {
         return ResponseEntity.ok(memberService.signUpMember(requestDTO));
     }
-
+    
+    @Operation(summary = "유저 리스트 조회", description = "유저 리스트를 조회합니다")
     @GetMapping
     public ResponseEntity<List<MemberDTO>> getAllMembers() {
         return ResponseEntity.ok(memberService.getAllMembers());
     }
 
+    @Operation(summary = "유저 삭제", description = "유저를 삭제합니다")
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse<Void>> deleteMember(@PathVariable int userId) {
         memberService.deleteMember(userId);
-        return ResponseEntity.ok(ApiResponse.onSuccess(null, "멤버 삭제 성공"));
+        return ResponseEntity.ok(ApiResponse.onSuccess(null, "유저 삭제 성공"));
     }
 }
