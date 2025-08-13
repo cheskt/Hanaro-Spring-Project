d0670bd441807b3ee631cb8935c6b7868681f991
cheskt <andy9793@naver.com>
2025-08-13 10:22:13 +0900

    batch

--- src/main/java/hanaro/config/BatchConfig.java
+++ src/main/java/hanaro/config/BatchConfig.java
@@ -1,45 +1,69 @@
 package hanaro.config;
 
-import hanaro.stats.service.DailySaleBatchService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import hanaro.order.entity.Orders;
+import hanaro.order.repository.OrdersRepository;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-@EnableBatchProcessing
 @RequiredArgsConstructor
 public class BatchConfig {
-
-    private final DailySaleBatchService dailySaleBatchService;
+    private final OrdersRepository orderRepository;
 
     @Bean
-    public Job dailySaleJob(JobRepository jobRepository, Step dailySaleStep) {
-        return new JobBuilder("dailySaleJob", jobRepository)
-                .start(dailySaleStep)
-                .build();
+    public Job csvJob(JobRepository jobRepository, Step step) {
+        return new JobBuilder("csvJob", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .start(step)
+            .build();
     }
 
     @Bean
-    public Step dailySaleStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("dailySaleStep", jobRepository)
-                .tasklet(dailySaleTasklet(), transactionManager)
-                .build();
+    public Step step(JobRepository jobRepository,
+        PlatformTransactionManager transactionManager, OrdersRepository orderRepository) {
+        return new StepBuilder("csvStep", jobRepository)
+            .<Orders, Orders>chunk(5, transactionManager)
+            .reader(orderReader())
+            .writer(orderWriter(orderRepository))
+            .build();
     }
 
     @Bean
-    public Tasklet dailySaleTasklet() {
-        return (contribution, chunkContext) -> {
-            dailySaleBatchService.runDaily();
-            return RepeatStatus.FINISHED;
-        };
+    @StepScope
+    protected FlatFileItemReader<Orders> orderReader() {
+        return new FlatFileItemReaderBuilder<Orders>()
+            .name("orderReader")
+            .resource(new ClassPathResource("orders.csv"))
+            .linesToSkip(1)
+            .delimited()
+            .names("orderText", "state")
+            .fieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
+                setTargetType(Orders.class);
+            }}).build();
     }
+
+    @Bean
+    public RepositoryItemWriter<Orders> orderWriter(OrdersRepository repository) {
+        return new RepositoryItemWriterBuilder<Orders>()
+            .repository(repository)
+            .methodName("save")
+            .build();
+    }
+
 }
--- src/main/java/hanaro/stats/service/DailySaleBatchService.java
+++ src/main/java/hanaro/stats/service/DailySaleBatchService.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
